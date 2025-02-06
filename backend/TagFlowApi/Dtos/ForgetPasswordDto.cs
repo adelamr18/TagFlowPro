adbd80460889c9a124de837bf7901cb9237086f1
adelamr18 <adelamr88@gmail.com>
2025-02-06 17:49:10 +0200

    Deploy to prod and remove backend project from frontend root folder

--- backend/TagFlowApi/Dtos/ForgetPasswordDto.cs
+++
@@ -1,8 +0,0 @@
-namespace TagFlowApi.Dtos
-{
-    public class ForgetPasswordDto
-    {
-        public string Email { get; set; } = "";
-        public string NewPassword { get; set; } = "";
-    }
-}
